51f17a0963fd7f0e11892f7d338afda14839d973
Rafael Silva de Oliveira <rafasdoliveira@outlook.com>
2024-01-11 11:12:26 -0300

    updates

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -29,14 +29,9 @@ export default function SignUp() {
     const [terms, setTerms] = useState('')
     const [password, setPassword] = useState('')
 
-
-    //Essa função é comumente utilizada em componentes React para gerenciar o estado de um campo de formulário.
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-      };
-
     //Função utilizada para registrar os dados no banco
     function register(){
+    
         axios.post('http://localhost:8081/users',{
             name: name,
             username: username,
@@ -64,6 +59,8 @@ export default function SignUp() {
           });
     }
 
+
+
     //Constantes para os selects
     const listGoal = {
         opcao: "Selecione uma  opção",
@@ -88,31 +85,43 @@ export default function SignUp() {
         opcao8: "Minha igreja não está cadastrada"
     };
 
+    const listGender = {
+        opcao: "Selecione uma  opção",
+        opcao1: "Feminino",
+        opcao2: "Masculino"
+    };
+
+    
+
     return(
         <div>
             <div>
-                <Title text='Inscreva-se' />
+                <Title title='Cadastre-se!' />
             </div>
             <div>
-                <Input label='Nome:' type='text' id='inName' placeholder='Nome e Sobrenome' onChange={(e) => setName(e.target.value)} />
-                <Input label='Nome de Usuário:' type='text' id='inUser' placeholder='Nome de Usuário' onChange={(e) => setUsername(e.target.value)} />
-                <Select name='goals' id='goals' text='Objetivo Principal' options={listGoal} onChange={(e) => setGoal(e.target.value)} />
-                <Input label='Data de Nascimento:' type='date' id='inBirthday'onChange={(e) => setBirthday(e.target.value)} />
-                <Input label='Telefone:' type='tel' id='inPhone'onChange={(e) => setPhone(e.target.value)} />
-                <Input label='E-mail:' type='email' id='inMail'onChange={(e) => setEmail(e.target.value)} />
-                <div className={styles.signupGender}>
-                    <label>Gênero:</label>
-                    <Input label='Masculino' type='radio' name='gender' id='inGender' 
-                    onChange={(e) => setGender(e.target.value)} />
-                    <Input label='Feminino' type='radio' name='gender' id='inGender' 
-                    onChange={(e) => setGender(e.target.value)} />                                   
-                </div>
-                <Input label='Peso atual (kg):' type='number' id='inWeight' min="1" step="0.1" onChange={(e) => setWeight(e.target.value)} />
-                <Input label='Altura (mt):' type='number' id='inHeight' min="1" step="0.1" onChange={(e) => setHeight(e.target.value)} />
-                <Select name='church' id='church' text='Qual sua igreja?' options={listChurch} onChange={(e) => setChurch(e.target.value)} />
-                <Input label='Defina uma senha:' type='password' id='inPassword' onChange={(e) => setPassword(e.target.value)} /> 
-                <Input label='Aceito os Termos e Condições' type='checkbox' id='inTerms' onChange={(e) => setTerms(e.target.value)} /> 
-                <Input type='submit' onClick={register} value='Registrar' />   
+                <form>
+                    <Input label='Nome:' type='text' id='inName' placeholder='Nome e Sobrenome' onChange={(e) => setName(e.target.value)} />
+                    <Input label='Nome de Usuário:' type='text' id='inUser' placeholder='Nome de Usuário' onChange={(e) => setUsername(e.target.value)} />
+                    <Input label='Data de Nascimento:' type='date' id='inBirthday'onChange={(e) => setBirthday(e.target.value)} />
+                    <Input label='Telefone:' type='tel' id='inPhone' placeholder='O mesmo do WhatsApp' onChange={(e) => setPhone(e.target.value)} />
+                    <Input label='E-mail:' type='email' id='inMail' placeholder='E-mail mais utlizado' onChange={(e) => setEmail(e.target.value)} />
+                    <Select name='inGoal' id='inGoal' text='Objetivo Principal' options={listGoal} onChange={(e) => setGoal(e.target.value)} />
+                    <Select name='inGender' id='inGender' text='Gênero' options={listGender} onChange={(e) => setGender(e.target.value)} />
+                    {/* Utilizar esse bloco uma outra hora */}
+                    {/* <div className={styles.signupGender}>
+                        <label>Gênero:</label>
+                        <Input label='Masculino' type='radio' name='gender' id='inGender' 
+                        onChange={(e) => setGender(e.target.value)} />
+                        <Input label='Feminino' type='radio' name='gender' id='inGender' 
+                        onChange={(e) => setGender(e.target.value)} />                                   
+                        </div>
+                        <Input label='Peso atual (kg):' type='number' id='inWeight' min="1" step="0.1" placeholder='Peso em quilos' onChange={(e) => setWeight(e.target.value)} />
+                    <Input label='Altura (mt):' type='number' id='inHeight' min="1" step="0.1" onChange={(e) => setHeight(e.target.value)} /> */}
+                    <Select name='inChurch' id='inChurch' text='Qual sua igreja?' options={listChurch} onChange={(e) => setChurch(e.target.value)} />
+                    <Input label='Defina uma senha:' type='password' id='inPassword' onChange={(e) => setPassword(e.target.value)} /> 
+                    {/* <Input label='Aceito os Termos e Condições' type='checkbox' id='inTerms' onChange={(e) => setTerms(e.target.value)} />  */}
+                    <Input type='submit' onClick={register} value='Registrar' />   
+                </form>
             </div>
             <div>
                 <Button text= "Voltar para página inicial" link="/"/>
